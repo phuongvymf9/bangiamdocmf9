6cbf0fff6d84eb2425e62d93bd9d75080939752c
Ly Ngoc Phuong Vy <phuongvy@mobifone.vn>
2019-06-03 13:49:43 +0700

    chỉnh sửa file app navigation, thêm createAppContainer do gặp lỗi khác phiên bản

---
+++ navigations/AppNavigations.js
@@ -0,0 +1,42 @@
+import { createSwitchNavigator, createStackNavigator, createAppContainer } from "react-navigation";
+import { SolidColors }           from "../const/Colors";
+
+import AppLoading                from "../screens/AppLoading";
+import MainStackNavigations      from "./MainStackNavigations";
+import LoginStackNavigation      from './LoginStackNavigation';
+
+const stackNaviConfig = {
+  navigationOptions: {
+    headerMode        : 'none',
+    headerStyle       : {
+      backgroundColor : SolidColors.appBarColor,
+    },
+    headerTintColor   : '#fff',
+    headerTitleStyle  : {
+      fontWeight      : 'bold',
+    },
+  }
+}
+
+//test
+const NavigationStack = createStackNavigator(
+  {
+    Loading         : AppLoading, // AppLoading,
+    Login           : LoginStackNavigation,
+    Home            : MainStackNavigations
+  },
+  stackNaviConfig
+);
+
+const Container = createAppContainer(NavigationStack);
+
+export default Container;
+
+// export default createSwitchNavigator(
+//   {
+//     Loading         : AppLoading, // AppLoading,
+//     Login           : LoginStackNavigation,
+//     Home            : MainStackNavigations
+//   },
+//   stackNaviConfig
+// );
