c80b64c3bf177764ffb73bf36e9341323dae6681
Ly Ngoc Phuong Vy <phuongvy@mobifone.vn>
2019-06-03 13:45:08 +0700

    file login

---
+++ comps/background/LoginBG.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {  StyleSheet } from 'react-native';
+import { LinearGradient } from "expo";
+
+import { GradientColors } from "../../const/Colors";
+
+export default class LoginBG extends Component {
+  render() {
+    return (
+      <LinearGradient
+        style   = {css.bgColor}
+        colors  = {GradientColors.backgroundColor.colors}
+        start   = {GradientColors.backgroundColor.start}
+      />
+    );
+  }
+}
+
+const css = StyleSheet.create({
+  bgColor: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  }
+});
