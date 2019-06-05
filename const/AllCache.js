2b703435449209b66f9763bb98c46b807a1ca0b1
Ly Ngoc Phuong Vy <phuongvy@mobifone.vn>
2019-06-05 10:25:50 +0700

    delete cac ID man hinh CSKH, TNXLTT, qrcode, BCHA

--- const/AllCache.js
+++ const/AllCache.js
@@ -6,44 +6,4 @@ export {
   MenuListCache, setMenuListCache, resetMenuListCache
 }
 
-//////////////////////////////////////////////////////////////////////////////
 
-var IDChuongTrinhCSKHCache    = null;
-const cacheIDChuongTrinhCSKH  = value => IDChuongTrinhCSKHCache = value;
-const resetIDChuongTrinhCSKH  = () => IDChuongTrinhCSKHCache = null;
-
-export {
-  IDChuongTrinhCSKHCache, cacheIDChuongTrinhCSKH, resetIDChuongTrinhCSKH
-}
-
-//////////////////////////////////////////////////////////////////////////////
-
-var IDManHinhTNXLYCCache    = null;
-const cacheIDManHinhTNXLYC  = value => IDManHinhTNXLYCCache = value;
-const resetIDManHinhTNXLYC  = () => IDManHinhTNXLYCCache = null;
-
-export {
-  IDManHinhTNXLYCCache, cacheIDManHinhTNXLYC, resetIDManHinhTNXLYC
-}
-
-//////////////////////////////////////////////////////////////////////////////
-// Du lieu quet QR Code DBL
-
-var qrDataDBL = null;
-const cacheQRDataDBL = value => qrDataDBL = value;
-const resetQRDataDBL = () => qrDataDBL = null;
-
-export {
-  qrDataDBL, cacheQRDataDBL, resetQRDataDBL
-}
-//////////////////////////////////////////////////////////////////////////////
-//Bao cao hinh anh
-var IDManHinhBCHACCache    = null;
-const cacheIDManHinhBCHA  = value => IDManHinhBCHACCache = value;
-const resetIDManHinhBCHA  = () => IDManHinhBCHACCache = null;
-
-export {
-  IDManHinhBCHACCache, cacheIDManHinhBCHA, resetIDManHinhBCHA
-}
-
-//////////////////////////////////////////////////////////////////////////////
