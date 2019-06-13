12e1233856c9edc381f233f9b47f4212fe78ab1d
Ly Ngoc Phuong Vy <phuongvy@mobifone.vn>
2019-06-13 17:02:16 +0700

    Test: Goi API lay danh sach chua duyet

--- screens/capsokhuyenkhich/CSKKDaDuyetScreen.js
+++ screens/capsokhuyenkhich/CSKKDaDuyetScreen.js
@@ -1,52 +1,88 @@
 import React, { PureComponent } from 'react';
-import { View, StyleSheet, ScrollView, RefreshControl, Text } from 'react-native';
+import { View, StyleSheet, Text, ScrollView, Alert, RefreshControl } from 'react-native';
 
-import { printLog, printError } from '../../utils/LogUtil';
-import { getUserObjectAsync } from '../../utils/StorageUtils';
+import { printLog, printError }         from '../../utils/LogUtil';
+import { getUserObjectAsync }           from '../../utils/StorageUtils';
 
-import { Layout } from '../../const/Layout';
+import { Layout }           from '../../const/Layout';
+import { SolidColors }      from '../../const/Colors';
 
-export default class CSKKDaDuyetScreen extends PureComponent {
-    state = {
-        refreshing : false,
-        listThongKeYC: [],
-        switchValue: false,
-        value: ''
-    };
-    render(){
-      return(
-        <View style = {[css.container, AlignStyle.middle]}>
-            <ScrollView 
-                style     = {css.main}
-                onScroll  = {this.handleScroll}
-                scrollEventThrottle = {16}
-                refreshControl  = {
-                    <RefreshControl
-                        refreshing  = {this.state.refreshing}
-                        onRefresh   = {this._onRefresh} />
-                }>
-                   <Text> Đã duyệt </Text>
-            </ScrollView>
-        </View>
-        );
-    }
+import CapSoKhuyenKhichApi  from '../../api/CapSoKhuyenKhichApi';
+import ListCSKK from '../../comps/capsokhuyenkhich/ListCSKK';
+
+export default class CSKKDaDuyetscreen extends PureComponent {
+  state = {
+    listCSKK     : null,
+    refreshing      : false,
+  };
+
+  async componentDidMount() {
+    this.getListCSKKChuaDuyet();
+  }
+
+  getListCSKKChuaDuyet  = async () => {
+    let userInfo = await getUserObjectAsync();
+
+    CapSoKhuyenKhichApi.MNG_CSKK_GetList(userInfo.userid,
+      reS => {
+        printLog('getListCSKKChuaDuyet', reS);
+        if (reS.result) {
+            /////////////////////////// Kiem tra danh sach rong ///////////////////////////
+            if(reS.result.length > 0){
+              let listCSKK = reS.result;
+              this.setState({ listCSKK: listCSKK });
+            } else {
+              this.setState({ listCSKK: "NODATA" });
+            }
+          } else if ((reS.error)){
+            Alert.alert('THÔNG BÁO', 'Rất tiếc! Đã xảy ra lỗi trong quá trình tải danh sách thuê bao.' + reS.error);
+          }
+          //this.setState({ loading: false }); // ngưng thông báo
+      },
+      reE => {
+        //this.setState({ loading: false }); // ngưng thông báo
+        Alert.alert('THÔNG BÁO', 'Rất tiếc! Đã xảy ra lỗi trong quá trình tải danh sách thuê bao.\nVui lòng thử lại sau.');
+        printError('getListCSKKChuaDuyet', reE);
+      }
+    );
+  }
+
+  render(){
+    return(
+      <View style = {[css.container, AlignStyle.middle]}>
+        <ScrollView 
+          style     = {css.main}
+          onScroll  = {this.handleScroll}
+          scrollEventThrottle = {16}
+          refreshControl  = {
+            <RefreshControl
+              refreshing  = {this.state.refreshing}
+              onRefresh   = {this._onRefresh}
+            />
+          }>
+            <ListCSKK
+                data         = {this.state.listCSKK}
+                navigate     = {this.props.navigation.navigate}
+                reloadListDB = {this.getListCSKKChuaDuyet} 
+             />
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 const css = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FFFFFF'
-    }, 
-    main: {
-        paddingVertical: Layout.marginPaddingDefault,
-    },
-    txtYeuCau: { 
-        color: '#398FFF', 
-        textAlign: 'center'
-    },
-    vMain: {
-        backgroundColor: '#FFFFFF',
-        borderRadius: Layout.borderRadisComponent,
-        marginBottom: Layout.marginPaddingDefault
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF'
+  },
+  txtChonHuyen: {
+    color: SolidColors.grey,
+    textAlign: 'center',
+    marginTop: Layout.marginPaddingDefault
+  },
+  txtNoData: {
+    textAlign: 'center',
+    color: SolidColors.grey
+  }
 });
