cda47d9580a6732f38e511d86d75fd3d6f451f9a
Ly Ngoc Phuong Vy <phuongvy@mobifone.vn>
2019-06-13 16:58:21 +0700

    Tao API lay danh sach goi cuoc

--- api/CapSoKhuyenKhichApi.js
+++ api/CapSoKhuyenKhichApi.js
@@ -16,8 +16,26 @@ function MNG_CSKK_GetList( userid, handleSuccess, handleError) {
     printLog('exec SOAP MNG_CSKK_GetList', { userid });
   
     ApiUtils.execSOAPWebServiceWithOperation(op, data, handleSuccess, handleError, { test: true });
+}
+
+function MNG_CSKK_GetListGoiCuoc( userid, handleSuccess, handleError) {
+    let op = 'MNG_CSKK_GetListGoiCuoc';
+  
+    let data = `<?xml version="1.0" encoding="utf-8"?>
+    <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+      <soap:Body>
+        <MNG_CSKK_GetListGoiCuoc xmlns="WSDBL">
+          <userid>${userid}</userid>
+        </MNG_CSKK_GetListGoiCuoc>
+      </soap:Body>
+    </soap:Envelope>`;
+  
+    printLog('exec SOAP MNG_CSKK_GetListGoiCuoc', { userid });
+  
+    ApiUtils.execSOAPWebServiceWithOperation(op, data, handleSuccess, handleError, { test: true });
   }
 
 export default {
-    MNG_CSKK_GetList
+    MNG_CSKK_GetList,
+    MNG_CSKK_GetListGoiCuoc
  }
