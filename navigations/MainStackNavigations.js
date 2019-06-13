8d28006467708967d25b3f53e67e1fb2c0d136e7
Ly Ngoc Phuong Vy <phuongvy@mobifone.vn>
2019-06-13 17:02:43 +0700

    import CSKK tab navigation, va man hinh CSKK chi tiet

--- navigations/MainStackNavigations.js
+++ navigations/MainStackNavigations.js
@@ -8,6 +8,12 @@ import TaiKhoanScreen           from "../screens/TaiKhoanScreen";
 import HistoryScreen            from "../screens/HistoryScreen";
 import DongBoScreen             from "../screens/DongBoScreen";
 
+// Cap so khuyen khich
+import CSKKTabNavigations       from "./CSKKTabNavigations";
+import CSKKChiTietScreen        from "../screens/capsokhuyenkhich/CSKKChiTietScreen";
+
+import ModalChung from "../comps/capsokhuyenkhich/ModalChung";
+
 const stackNaviConfig = {
   navigationOptions: {
     headerMode        : 'none',
@@ -32,6 +38,14 @@ export default createStackNavigator(
     UserInfo      : TaiKhoanScreen,
     LichSuChamSoc : HistoryScreen,
     DongBo        : DongBoScreen,
+
+    // ## Cap so khuyen khich =================================
+
+    CapSoKhuyenKhich: {
+      screen: CSKKTabNavigations,
+      navigationOptions: { title: "Cấp số khuyến khích" }
+    },
+    CSKKChiTiet: CSKKChiTietScreen,
   },
   stackNaviConfig
 );
