38897461d54af84fa8c51e08e283f411300f48cc
Ly Ngoc Phuong Vy <phuongvy@mobifone.vn>
2019-06-17 16:55:51 +0700

    Delete stackNaviConfig, gom chung navigation vao tung tab do khong hieu duoc stackNa....

--- navigations/MainStackNavigations.js
+++ navigations/MainStackNavigations.js
@@ -12,25 +12,10 @@ import DongBoScreen             from "../screens/DongBoScreen";
 import CSKKTabNavigations       from "./CSKKTabNavigations";
 import CSKKChiTietScreen        from "../screens/capsokhuyenkhich/CSKKChiTietScreen";
 
-import ModalChung from "../comps/capsokhuyenkhich/ModalChung";
+// Cap so khuyen khich
+import KPSTabNavigations       from "./KPSTabNavigations";
+import KPSChiTietScreen        from "../screens/khoiphucso/KPSChiTietScreen";
 
-const stackNaviConfig = {
-  navigationOptions: {
-    headerMode        : 'none',
-    headerStyle       : {
-      backgroundColor : SolidColors.appBarColor,
-    },
-    headerTintColor   : '#fff',
-    headerTitleStyle  : {
-      fontWeight      : 'bold',
-    },
-  },
-  ...Platform.select({
-    android: {
-      transitionConfig: getSlideFromRightTransitionConfig
-    }
-  })
-}
 
 export default createStackNavigator(
   {
@@ -39,13 +24,51 @@ export default createStackNavigator(
     LichSuChamSoc : HistoryScreen,
     DongBo        : DongBoScreen,
 
-    // ## Cap so khuyen khich =================================
+// ## Cap so khuyen khich =================================
 
     CapSoKhuyenKhich: {
       screen: CSKKTabNavigations,
-      navigationOptions: { title: "Cấp số khuyến khích" }
+      navigationOptions: { 
+        title: "Cấp số khuyến khích" ,
+        headerMode        : 'none',
+        headerStyle       : {
+          backgroundColor : SolidColors.appBarColor,
+        },
+        headerTintColor   : '#fff',
+        headerTitleStyle  : {
+          fontWeight      : 'bold',
+        },
+      },
+      ...Platform.select({
+        android: {
+          transitionConfig: getSlideFromRightTransitionConfig
+        }
+      })
     },
     CSKKChiTiet: CSKKChiTietScreen,
+    
+// ## Khoi phuc so =================================
+
+     KhoiPhucSo: {
+      screen: KPSTabNavigations,
+      navigationOptions: { 
+        title: "Khôi phục số" ,
+        headerMode        : 'none',
+        headerStyle       : {
+          backgroundColor : SolidColors.appBarColor,
+        },
+        headerTintColor   : '#fff',
+        headerTitleStyle  : {
+          fontWeight      : 'bold',
+        },
+      },
+      ...Platform.select({
+        android: {
+          transitionConfig: getSlideFromRightTransitionConfig
+        }
+      })
+    },
+    KPSChiTiet: KPSChiTietScreen,
   },
-  stackNaviConfig
+  //stackNaviConfig
 );
