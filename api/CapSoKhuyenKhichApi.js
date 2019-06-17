f0e143858b511ea8f5f3f31f39240fe6b5ae975f
Ly Ngoc Phuong Vy <phuongvy@mobifone.vn>
2019-06-17 13:47:11 +0700

    Thay doi API lay danh sach CSKK them bien daduyet. Goi API lay danh sach khoi phuc so

--- api/CapSoKhuyenKhichApi.js
+++ api/CapSoKhuyenKhichApi.js
@@ -1,7 +1,7 @@
 import ApiUtils from "./ApiUtils";
 import { printLog } from "../utils/LogUtil";
 
-function MNG_CSKK_GetList( userid, handleSuccess, handleError) {
+function MNG_CSKK_GetList( userid, daduyet, handleSuccess, handleError) {
     let op = 'MNG_CSKK_GetList';
   
     let data = `<?xml version="1.0" encoding="utf-8"?>
@@ -9,11 +9,12 @@ function MNG_CSKK_GetList( userid, handleSuccess, handleError) {
       <soap:Body>
         <MNG_CSKK_GetList xmlns="WSDBL">
           <userid>${userid}</userid>
+          <daduyet>${daduyet}</daduyet>
         </MNG_CSKK_GetList>
       </soap:Body>
     </soap:Envelope>`;
   
-    printLog('exec SOAP MNG_CSKK_GetList', { userid });
+    printLog('exec SOAP MNG_CSKK_GetList', { userid, daduyet });
   
     ApiUtils.execSOAPWebServiceWithOperation(op, data, handleSuccess, handleError, { test: true });
 }
@@ -33,9 +34,28 @@ function MNG_CSKK_GetListGoiCuoc( userid, handleSuccess, handleError) {
     printLog('exec SOAP MNG_CSKK_GetListGoiCuoc', { userid });
   
     ApiUtils.execSOAPWebServiceWithOperation(op, data, handleSuccess, handleError, { test: true });
-  }
+}
+
+function MNG_KPS_GetList( userid, daduyet, handleSuccess, handleError) {
+  let op = 'MNG_KPS_GetList';
+
+  let data = `<?xml version="1.0" encoding="utf-8"?>
+  <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+    <soap:Body>
+      <MNG_KPS_GetList xmlns="WSDBL">
+        <userid>${userid}</userid>
+        <daduyet>${daduyet}</daduyet>
+      </MNG_KPS_GetList>
+    </soap:Body>
+  </soap:Envelope>`;
+
+  printLog('exec SOAP MNG_KPS_GetList', { userid, daduyet });
+
+  ApiUtils.execSOAPWebServiceWithOperation(op, data, handleSuccess, handleError, { test: true });
+}
 
 export default {
     MNG_CSKK_GetList,
-    MNG_CSKK_GetListGoiCuoc
- }
+    MNG_CSKK_GetListGoiCuoc,
+    MNG_KPS_GetList
+}
