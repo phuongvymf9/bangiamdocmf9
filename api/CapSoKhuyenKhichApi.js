6238bb8b33a34941a072ae6ea75d6c2a8a7a87a7
Ly Ngoc Phuong Vy <phuongvy@mobifone.vn>
2019-06-20 14:18:40 +0700

    thêm loại kh vào API lấy list gói cước

--- api/CapSoKhuyenKhichApi.js
+++ api/CapSoKhuyenKhichApi.js
@@ -19,7 +19,7 @@ function MNG_CSKK_GetList( userid, daduyet, handleSuccess, handleError) {
     ApiUtils.execSOAPWebServiceWithOperation(op, data, handleSuccess, handleError, { test: true });
 }
 
-function MNG_CSKK_GetListGoiCuoc( userid, handleSuccess, handleError) {
+function MNG_CSKK_GetListGoiCuoc( userid, loaikh, handleSuccess, handleError) {
     let op = 'MNG_CSKK_GetListGoiCuoc';
   
     let data = `<?xml version="1.0" encoding="utf-8"?>
@@ -27,11 +27,12 @@ function MNG_CSKK_GetListGoiCuoc( userid, handleSuccess, handleError) {
       <soap:Body>
         <MNG_CSKK_GetListGoiCuoc xmlns="WSDBL">
           <userid>${userid}</userid>
+          <loaikh>${loaikh}</loaikh>
         </MNG_CSKK_GetListGoiCuoc>
       </soap:Body>
     </soap:Envelope>`;
   
-    printLog('exec SOAP MNG_CSKK_GetListGoiCuoc', { userid });
+    printLog('exec SOAP MNG_CSKK_GetListGoiCuoc', { userid, loaikh });
   
     ApiUtils.execSOAPWebServiceWithOperation(op, data, handleSuccess, handleError, { test: true });
 }
@@ -54,8 +55,39 @@ function MNG_KPS_GetList( userid, daduyet, handleSuccess, handleError) {
   ApiUtils.execSOAPWebServiceWithOperation(op, data, handleSuccess, handleError, { test: true });
 }
 
+function MNG_CSKK_Duyet( userid, idyeucau, checkduyet, lydo, goi_camket, magoi_khac, 
+                         tengoi_khac, muccuoc_khac, tg_camketkhac, capquanly, loaikh, handleSuccess, handleError) {
+  let op = 'MNG_CSKK_Duyet';
+
+  let data = `<?xml version="1.0" encoding="utf-8"?>
+  <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+    <soap:Body>
+      <MNG_CSKK_Duyet xmlns="WSDBL">
+        <userid>${userid}</userid>
+        <idyeucau>${idyeucau}</idyeucau>
+        <checkduyet>${checkduyet}</checkduyet>
+        <lydo>${lydo}</lydo>
+        <goi_camket>${goi_camket}</goi_camket>
+        <magoi_khac>${magoi_khac}</magoi_khac>
+        <tengoi_khac>${tengoi_khac}</tengoi_khac>
+        <muccuoc_khac>${muccuoc_khac}</muccuoc_khac>
+        <tg_camketkhac>${tg_camketkhac}</tg_camketkhac>
+        <capquanly>${capquanly}</capquanly>
+        <loaikh>${loaikh}</loaikh>
+      </MNG_CSKK_Duyet>
+    </soap:Body>
+  </soap:Envelope>`;
+
+  printLog('exec SOAP MNG_CSKK_Duyet', { userid, idyeucau, checkduyet, lydo, goi_camket, magoi_khac, 
+                                         tengoi_khac, muccuoc_khac, tg_camketkhac, capquanly, loaikh });
+
+  ApiUtils.execSOAPWebServiceWithOperation(op, data, handleSuccess, handleError, { test: true });
+}
+
+
 export default {
     MNG_CSKK_GetList,
     MNG_CSKK_GetListGoiCuoc,
-    MNG_KPS_GetList
+    MNG_KPS_GetList,
+    MNG_CSKK_Duyet
 }
